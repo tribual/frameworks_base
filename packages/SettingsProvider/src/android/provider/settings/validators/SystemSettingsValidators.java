b53d1302f63bd8172449698f3031aab6b1bd5a39
Marko Man <darkobas@gmail.com>
2019-10-08 11:03:31 +0200

    base: qs status bar header image [1/4]
    -- Genkzsz11: Adapt to R
    
    Bootleggers A13/A14 edit: Use the layout made by minaripenguin, but keep the core from omnirom
    reference: https://github.com/minaripenguin/android_frameworks_base/commit/8f695592d05aa44a37ba25e04030fed081e7636b
    
    Also added a fork of Android-FadingEdgeLayout
    It allows to do the fade-out without having to use hacky gradients and masks (than can break tinted bitmaps)
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -16,7 +16,6 @@
 
 package android.provider.settings.validators;
 
-import static android.provider.settings.validators.SettingsValidators.ANY_INTEGER_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.ANY_STRING_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.BOOLEAN_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.COMPONENT_NAME_VALIDATOR;
